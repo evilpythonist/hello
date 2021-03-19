2ebb724a1cb034c497a1b484a11ccc9d4a920670
RP-pl <68811106+RP-pl@users.noreply.github.com>
2021-03-19 18:13:48 +0100

    Update commands.py

--- commands.py
+++ commands.py
@@ -1,6 +1,8 @@
 from datetime import datetime
 
-from discord import Message
+import discord
+from discord import Message, Client, Member
+from discord.utils import get
 
 
 async def wiki(message:Message,args):
@@ -11,3 +13,25 @@ async def wiki(message:Message,args):
     await message.channel.send("https://pl.wikipedia.org/wiki/" + str)
 async def now(message:Message):
     await message.channel.send(datetime.now())
+
+async def mute(message:Message):
+    await message.channel.send(content='No exe')
+    overwrite = discord.PermissionOverwrite()
+    overwrite.send_messages = False
+    overwrite.read_messages = True
+    await message.channel.set_permissions(message.author,overwrite=overwrite)
+    
+async def give_permission(user,message:Message):
+    overwrite = discord.PermissionOverwrite()
+    overwrite.send_messages = True
+    overwrite.read_messages = True
+    await message.channel.set_permissions(user,overwrite=overwrite)
+    
+async def give_permission(client,id):
+    print(client.guilds[0].members)
+    member =  get(client.guilds[0].members, id=int(id[0]))
+async def kick(client,id):
+    member = get(client.guilds[0].members, id=int(id[0]))
+    member.kick()
+async def invite(client:Client,message:Message):
+    await message.channel.send("https://discord.gg/r8w7XAgH2E")
