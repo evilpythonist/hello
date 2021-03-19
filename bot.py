052d8ff952394cdf90eb3e6b0c53f201b7885597
RP-pl <68811106+RP-pl@users.noreply.github.com>
2021-03-19 14:50:32 +0100

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,13 @@
+import discord
+
+class CustomClient(discord.Client):
+    async def on_ready(self):
+        print(f'{self.user} connected')
+    async def on_member_join(self,member:discord.Member):
+        await member.create_dm()
+        await member.dm_channel.send('Hello')
+    async def on_message(self,message:discord.Message):
+        if message.author == self.user:
+            pass
+client = CustomClient()
+client.run("ODIyNDU3NTAyNzM3ODkxMzI5.YFSjPQ.sooMBr1YZGg3mbaFKw98vCiJl3A")
